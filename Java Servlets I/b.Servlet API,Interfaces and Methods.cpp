f624b1cbe1dd64f8771b0dd1ed4a53f01f2eecdc
Subhash Mangale <58906689+SubhashMangale@users.noreply.github.com>
2022-09-27 15:33:10 +0530

    Create b.Servlet API,Interfaces and Methods.cpp

---
+++ Java Servlets I/b.Servlet API,Interfaces and Methods.cpp
@@ -0,0 +1,12 @@
+1.Servlet API:
+Servlet API contains so many classes and Interfaces which describe contract between: 1) Servlet class 2) RTE provided for an instace by confirming servlet container.
+Servlet API containes following 2 packages which contains its classes and interfaces:
+1) javax.servlets.*;    =>   classes and interfaces which describes contract between servlet class and RTE provided for an instance of a class by conforming servlet
+                             container.
+  
+2)javax.servlet.http.*; =>  classes and interfaces which describes contract between servlet class running under HTTP protocol and RTE provided for an instance of a 
+                            such class by conforming servlet container.
+--------------------------------------------**********************************************************************************----------------------------------------
+                              
+2.Servlet Interfaces and classes: https://drive.google.com/file/d/1khhtpC00h5O2wAwpCaTxFyPX8fYFDT3O/view?usp=sharing
+
